9dd2eaea6c89536996926e52dda56d4026eabaa7
Klaus Hörmann-Engl <klaus.hoermann-engl@world-direct.at>
2019-01-15 15:18:01 +0100

    [TASK] Adding constants and settings

---
+++ Configuration/TypoScript/constants.ts
@@ -0,0 +1,10 @@
+#customcategory=stagenote=LLL:EXT:stagenote/Resources/Private/Language/locallang_db.xlf:constants_category
+
+plugin.tx_stagenote {
+  settings {
+    #cat=stagenote/settings/includeBootstrap4; type=boolean; label=LLL:EXT:stagenote/Resources/Private/Language/locallang_db.xlf:constants_includeBootstrap4
+    includeBootstrap4 = 0
+    #cat=stagenote/settings/displayType; type=string[alert,toast]; label=LLL:EXT:stagenote/Resources/Private/Language/locallang_db.xlf:constants_displayType
+    displayType = alert
+  }
+}
--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -1,7 +1,8 @@
 plugin.tx_stagenote {
-    view {
-    }
-    settings {
-        includeBootstrap4 = 0
-    }
+  view {
+  }
+  settings {
+    includeBootstrap4 = {$plugin.tx_stagenote.settings.includeBootstrap4}
+    displayType = {$plugin.tx_stagenote.settings.displayType}
+  }
 }
